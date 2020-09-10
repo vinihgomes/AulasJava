2cab98252908dca5b0eaeeebdfb9aa7ca7a7b31d
vinihgomes <contatoviniciusgomes@gmail.com>
2020-09-10 19:07:01 -0300

    Atualização classes Java: Laços, condicionais, variáveis

---
+++ sintaxe-variaveis-e-fluxo/src/Multiplos3.java
@@ -0,0 +1,13 @@
+
+public class Multiplos3 {
+
+	public static void main(String[] args) {
+		for (int numero = 0 ; numero <= 100 ; numero++) {
+			if (numero %3 == 0) {
+				System.out.println(numero + " ");
+			}
+		}
+
+	}
+
+}
